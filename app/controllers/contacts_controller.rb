671d1cd0f7787fdfeff605339321d05a8ed247af
Susanna Miller <susanna@theironyard.com>
2016-03-08 15:15:25 -0500

    Worked on @contacts
    Added to contacts controller, new contact form, etc. Really struggling
    with getting contacts form to keep anything but final form field.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,13 +1,11 @@
 class ContactsController < ApplicationController
 	def new
 		@student = Student.find (params[:student_id])
+		@contacts = Array.new(2) { @student.contacts.new }
 		@contact = @student.contacts.new
 	end
 	def create
-		@student = Student.find (params[:student_id])
-		@contact = @student.contacts.create!(contact_params)
-		@contact_params = contact_params
-		#redirect_to student_path(@student) 
+		 
 	end	
 	def contact_params
 		params.require(:contact).permit(:category, :info, :student_id, :id)
