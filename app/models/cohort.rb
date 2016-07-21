d29a70c2f178b297503a6870f0ea3941ab5aa480
Susanna Miller <susanna@theironyard.com>
2016-07-21 15:09:05 -0400

    Clarified cohort model, updated student comments

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,4 +1,4 @@
-#The class "Cohort" inherits from the ActiveRecord Base
+#The model "Cohort" inherits from the ActiveRecord Base - includes info like methods for .find, .all, etc
 class Cohort < ActiveRecord::Base
 	#Each cohort contains (has a relationship to) mutiple students
 	has_many :students
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,10 +1,20 @@
+#Defines class "student" as inheriting from Active Record base
 class Student < ActiveRecord::Base
+	#Defines relationship between "student" and "jobs" - each student has (potentially) mutiple jobs, which are dependent on the student, and are destroyed if it is
 	has_many :jobs, dependent: :destroy
+	#student also takes on the nested attributes associated with each of the jobs under it
 	accepts_nested_attributes_for :jobs
+	#defines relationship between contacts and students. Each student has multiple contacts that are dependant on it and are destroyed if it is
 	has_many :contacts, dependent: :destroy
+	#student also takes on/allows the attributes of the contacts associated with it
 	accepts_nested_attributes_for :contacts
+	#Defines relationship between student and cohort - each student belongs to one cohort
 	belongs_to :cohort
+	#Defines relationship between student and course - each student belongs to one course
 	belongs_to :course
+	#defines feature of student class: it has an attached photo, which as 2 possible styles (sizes defined), and a default location to go to if there isn't one
 	has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "missing.png"
-  	validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+		#defines qualities of the attached files - that it's a photo, and is an image file
+		validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+#wraps up student class
 end
